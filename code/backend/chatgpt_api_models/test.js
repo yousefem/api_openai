2e554f46f00189fe47a27566c5231df893db9d36
Yousef <128886569+yousefem@users.noreply.github.com>
2023-07-30 23:25:49 +0000

    Pending changes exported from your codespace

--- code/backend/chatgpt_api_models/test.js
+++ code/backend/chatgpt_api_models/test.js
@@ -1,1 +1,8 @@
-
+import { Configuration, OpenAIApi } from "openai";
+console.log(process);
+// const configuration = new Configuration({
+//     organization: "YOUR_ORG_ID",
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
